1d35320b4414e41c24195f228737298a44cbc93f
Alison Sabuwala <alison.sabuwala1024@gmail.com>
2022-10-07 04:30:29 +0000

    Add code to read images from tello and also record to video.avi. Images are sent to tello/image_raw. Some garbage Strings are sent as test objects to test/raw as well. Disregard that. we can remove that later

--- Examples/ROS/ORB_SLAM2/src/ros_tello.py
+++ Examples/ROS/ORB_SLAM2/src/ros_tello.py
@@ -29,6 +29,49 @@ import logging
 #Step 2 publish images to node /camera/image_raw
 #To do 
 
+tello = Tello(skipAddressCheck=True)
+tello.LOGGER.setLevel(logging.INFO)
+tello.connect()
+keepRecording = Event()
+keepRecording.set()
+
+tello.streamon()
+frame_read = tello.get_frame_read()
+bridge = CvBridge()
+
+
+
+# tello.takeoff()
+# cv2.imwrite("picture.png", frame_read.frame)
+
+# time.sleep(3)
+
+# cv2.imwrite("picture2.png", frame_read.frame)
+
+# tello.move("up", 80)
+# cv2.imwrite("picture3.png", frame_read.frame)
+
+# tello.move("forward", 50)
+# cv2.imwrite("picture4.png", frame_read.frame)
+
+# tello.move("left", 30)
+# cv2.imwrite("picture5.png", frame_read.frame)
+
+# tello.move("right", 60)
+# cv2.imwrite("picture6.png", frame_read.frame)
+
+# tello.move("left", 30)
+# cv2.imwrite("picture7.png", frame_read.frame)
+
+# tello.move("up", 20)
+# cv2.imwrite("picture8.png", frame_read.frame)
+
+# tello.move("back", 50)
+# cv2.imwrite("picture9.png", frame_read.frame)
+
+
+# tello.land()
+
 
 
 
@@ -73,6 +116,52 @@ def main():
         time.sleep(0.5)
         pub.publish("hello world at {} times".format(i))
         pub_img_test.publish(image)
+
+    def videoRecorder():
+        # create a VideoWrite object, recoring to ./video.avi
+        height, width, _ = frame_read.frame.shape
+        
+        print("Start recording")
+        video = cv2.VideoWriter('video.avi', cv2.VideoWriter_fourcc(*'MJPG'), 30, (width, height))
+
+        while keepRecording.is_set():
+            img_msg = bridge.cv2_to_imgmsg(frame_read.frame, encoding='rgb8')
+            pub_img_test.publish(img_msg)
+            video.write(frame_read.frame)
+            time.sleep(1 / 60)
+
+        print("Stop recording")
+
+        video.release()
+
+    # we need to run the recorder in a seperate thread, otherwise blocking options
+    #  would prevent frames from getting added to the video
+
+    time.sleep(2)
+
+    recorder = Thread(target=videoRecorder)
+    recorder.start()
+
+    def exit_handler(signum, frame):
+        msg = "Stopping drone. Drone will now hover. Please shutdown manually by pressing the button on the drone"
+        print(msg, end="", flush=True)
+        keepRecording.clear()
+        tello.streamoff()
+        recorder.join()
+        exit(1)
+
+    signal.signal(signal.SIGINT, exit_handler)
+
+
+    while recorder.is_alive():
+        time.sleep(0.2)
+
+    if (keepRecording.is_set() == True):
+        keepRecording.clear()
+        tello.streamoff()
+
+    recorder.join()
+    
     rospy.spin()
   
 if __name__ == '__main__':
